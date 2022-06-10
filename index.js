d1c1a731b7586de89091171a8e90dce038eb178c
whiterqbbit <wedd13@gmail.com>
2022-06-10 14:55:49 +0200

    feat: first init

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
