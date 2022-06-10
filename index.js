110084ec4dc7d42bce51ab67659eb439e0c93652
whiterqbbit <wedd13@gmail.com>
2022-06-10 15:10:05 +0200

    feat: exo fini

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ var express = require('express');
 var app = express();
 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+	res.json({ greeting: 'hello API' });
 });
 
-
+app.get('/api/whoami', function (req, res) {
+	let software = req.get('User-Agent');
+	let language = req.get('Accept-Language');
+	let ip = req.ip;
+	res.json({ ipaddress: ip, language: language, software: software });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
